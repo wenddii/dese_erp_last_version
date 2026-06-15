b25850b6bd8bb2f56a0deb8820824a2ac86651a7
wenddii <wendwosen03@gmail.com>
2026-06-15 09:33:35 +0300

    fixed the add to cart view

--- cart/views.py
+++ cart/views.py
@@ -2,27 +2,56 @@ from django.shortcuts import redirect
 from inventory.models import Product
 from sales.models import Sale
 from django.db import transaction
+from django.contrib import messages
 
 
 def add_cart(request, product_id):
 
+    product = Product.objects.get(id=product_id)
+
     qty = int(request.POST.get("qty", 1))
+
+    if qty <= 0:
+        messages.error(request, "Quantity must be greater than zero.")
+        return redirect("sales_manager")
 
     cart = request.session.get("cart", {})
 
     key = str(product_id)
 
+    current_qty = cart.get(key, {}).get("qty", 0)
+
+    new_total_qty = current_qty + qty
+
+    if new_total_qty > product.current_stock:
+
+        messages.error(
+            request,
+            f"Only {product.current_stock} units of {product.name} are available."
+        )
+
+        return redirect("sales_manager")
+
     if key in cart:
-        cart[key]["qty"] += qty
+
+        cart[key]["qty"] = new_total_qty
 
     else:
+
         cart[key] = {
             "qty": qty
         }
 
     request.session["cart"] = cart
 
+    messages.success(
+        request,
+        f"{qty} x {product.name} added to cart."
+    )
+
     return redirect("sales_manager")
+
+
 
 def remove_cart(request, product_id):
 
