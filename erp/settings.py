659ca735ce9d2086ff696a2dbf040ee27c6855a2
wenddii <wendwosen03@gmail.com>
2026-06-16 10:08:14 +0300

    added cloudinary for images

--- erp/settings.py
+++ erp/settings.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import os
+import cloudinary
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 if os.path.exists(BASE_DIR / ".env"):
     load_dotenv(BASE_DIR / ".env")
-print("DB_HOST =", os.getenv("DB_HOST"))
 # -----------------------
 # CORE SECURITY SETTINGS
 # -----------------------
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'finance',
     'dashboard',
     'cart',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 # -----------------------
@@ -123,8 +125,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+cloudinary.config(
+cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+api_key=os.getenv("CLOUDINARY_API_KEY"),
+api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+secure=True
+)
